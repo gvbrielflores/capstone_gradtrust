b78921427b2f485b14c64adde7bc24f95f3ba3ad
gvbrielflores <gabrielf.lovo@gmail.com>
2025-02-20 18:32:13 -0600

    initial hardhat setup and initial github actions setup.

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Contract = await hre.ethers.getContractFactory("YourContract");
+  const contract = await Contract.deploy();
+
+  await contract.deployed();
+  console.log(`Contract deployed to: ${contract.address}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});

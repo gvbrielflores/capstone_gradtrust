1375e65e81ccacee8bd77b1c38844423647f17ea
gvbrielflores <gabrielf.lovo@gmail.com>
2025-02-27 23:50:14 -0600

    Started changing contract to comply with new architecture. architecture is now done; TODO - finish updating contract

--- contracts/firstcontract.sol
+++ contracts/firstcontract.sol
@@ -6,18 +6,25 @@ contract CredentialVerification {
         bytes32 signedCredentialaHash;
         address holder;
         address issuer;
+        string dataString;
         uint256 issuedAt;
     }
 
-    mapping(bytes32 => Credential) public credentials;
+    // Storage mappings
+    mapping(bytes32 => Credential) private credentials;
+    mapping(address => bool) public verifiedIssuers;
+    mapping(address => bytes32[]) private holderCredentials;
 
+    // Events
     event CredentialStored(bytes32 indexed signedCredentialHash, address indexed issuer, address indexed holder, 
-    uint256 issuedAt);
+    uint256 issuedAt, string data);
+    event IssuerRegistered(address indexed issuer);
+    event IssuerRevoked(address indexed issuer);
 
-    function storeSignedCredentialHash(bytes32 _signedCredentialHash, address _holder, uint256 _issuedAt) public {
-        require(credentials[_signedCredentialHash].issuer == address(0), "Diploma already exists");
-        credentials[_signedCredentialHash] = Credential(_signedCredentialHash, msg.sender, _holder, _issuedAt);
-        emit CredentialStored(_signedCredentialHash, msg.sender, _holder, _issuedAt);
+    function storeSignedCredentialHash(bytes32 _signedCredentialHash, address _holder, uint256 _issuedAt, string memory _dataString) public {
+        require(credentials[_signedCredentialHash].issuer == address(0), "Credential already exists");
+        credentials[_signedCredentialHash] = Credential(_signedCredentialHash, msg.sender, _holder, _dataString, _issuedAt);
+        emit CredentialStored(_signedCredentialHash, msg.sender, _holder, _issuedAt, _dataString);
     }
 
     function verifyCredential(bytes32 _diplomaHash) public view returns (bool) {

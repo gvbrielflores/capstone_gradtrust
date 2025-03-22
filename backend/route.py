d48e0da53a8b73d381a56a06d4cb267a36afc42d
gvbrielflores <gabrielf.lovo@gmail.com>
2025-03-21 19:30:54 -0500

    cleaning up backend. deleted redundant files

--- backend/route.py
+++
@@ -1,9 +0,0 @@
-from flask import request
-from models import issuers, accounts
-
-def register_routes(app ,db):
-    @app.route('/')
-    def index():
-        all_issuers = issuers.query.all()
-        return str(all_issuers)
-    
